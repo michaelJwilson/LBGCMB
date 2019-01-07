437c133f3f5d7ad0aabc9a154dc17fc04fe7209d
Mike Wilson <mjw@roe.ac.uk>
2019-01-07 22:48:11 +0000

    Cleaned up Reddy.py and added facility to return sample stats for given R depth [19.0, 26.0].

--- dropouts/reddy.py
+++ dropouts/reddy.py
@@ -1,15 +1,21 @@
 from __future__ import division
 
+import numpy as np
+
 
 def load_tables(printit = False):
   import  os
   import  pandas as pd
 
-
   root      = os.environ['LBGCMB']
   root     += '/dropouts/dat/reddy/'
 
   ## Size in arcmin^2.
+  ## Notes:
+  ##         In Table 1, NXX are photometric candidates, not those spectroscopically confirmed.
+  ##         In Table 3, R is the lower limit on the mag bin (typically, 0.5 in width excepth for the first which is 1.0 in width.)
+  ##         Mixture of columns from Table 2 and 3, which share common row definitions.
+
   tab_one   = pd.read_table(root + 'tabone.dat',   skiprows=1, sep=r"\s*", names=['Field', 'Size', 'NBX', 'NLBG'], engine='python')  
   tab_three = pd.read_table(root + 'tabthree.dat', skiprows=1, sep=r"\s*", names=['R', 'NBX_phot', 'NBX_spec', 'NBX_int', 'NBX_fAGN',\
                                                                                   'NBX_fint', 'NLBG_phot', 'NLBG_spec', 'NLBG_int',\
@@ -23,49 +29,68 @@ def load_tables(printit = False):
 
   return tab_one, tab_three
 
-def samplestats(printit = False):
-  """
+def samplestats(mag=23., printit=False):
+  '''
   Load specifications of Reddy BX and LBG samples from tabular data and create a dictionary containing (interloper free) gals. per sq. deg. 
-  """
+  '''
 
-  tab_one, tab_three   = load_tables(printit)
-  
-  stats                = {'BX': {'z': 2.0, 'frac_AGN': 0.03}, 'LBG': {'z': 3.0, 'frac_AGN': 0.02}}
+  tab_one, tab_three = load_tables(printit=printit)  
+  stats              = {'BX': {'z': 2.3, 'frac_AGN': 0.03, 'schechter': {}}, 'LBG': {'z': 3.05, 'frac_AGN': 0.02, 'schechter': {}}}
 
   for survey in ['BX', 'LBG']:      
-      stats[survey]['N']                   = tab_one['N' + survey].sum() 
-      
-      stats[survey]['TotalArea [deg^2]']   = tab_one['Size'][tab_one['N' + survey].notnull()].sum()/60.**2.
-      stats[survey]['frac_interloper']     = tab_three['N' + survey + '_int'].sum() / tab_three['N' + survey + '_spec'].sum()   
+    if mag < 19.0:
+      raise ValueError('Brightest characteristics available for Reddy are R > 19.0')
 
-      ## [deg^2]
-      stats[survey]['nbar']                =                                         stats[survey]['N']/stats[survey]['TotalArea [deg^2]']
+    elif mag < 22.:  
+      index                              = 0  ## First bin is 19.0 < 22.0
 
-      ## Are AGN already counted in interloper fraction?
-      stats[survey]['nbar_nointerlopers']  = (1. - stats[survey]['frac_interloper'])*stats[survey]['N']/stats[survey]['TotalArea [deg^2]']
+    elif mag <= 25.9:
+      ##  Upper limit to the bin is tab_three['R'] + 0.5;
+      index                              = np.where(np.abs(np.array(tab_three['R']) + 0.5 - mag).min() == np.abs(np.array(tab_three['R']) + 0.5 - mag))[0][0]
 
-      if printit:
-        print "\n\n%s Survey Specifications:\n" % survey
+    else:
+      raise  ValueError('Faintest characteristics available for Reddy are R < 26.0')
 
-        for k, v in stats[survey].iteritems():
-          print "%s \t\t %s" % (k.ljust(20), v)
+    stats[survey]['Rlim']                = np.array(tab_three['R'])[index] + 0.5
+    
+    stats[survey]['N']                   = np.array(tab_three['N' + survey + '_phot'])[:(1 + index)].sum()             
+    stats[survey]['TotalArea [deg^2]']   = tab_one['Size'][tab_one['N' + survey].notnull()].sum() / 60.**2.
+    stats[survey]['frac_interloper']     = np.array(tab_three['N' + survey + '_int'])[:(1 + index)].sum() / np.array(tab_three['N' + survey + '_spec'])[:(1 + index)].sum()
 
-  """
-  ## Schechter fn. parameterisation of the Reddy luminosity fn., phi(L) at z = 3. 
+    ## [deg^2]
+    stats[survey]['nbar']                = stats[survey]['N'] / stats[survey]['TotalArea [deg^2]']
+
+    ##  Are AGN already counted in interloper fraction?
+    stats[survey]['nbar_nointerlopers']  = (1. - stats[survey]['frac_interloper']) * stats[survey]['N'] / stats[survey]['TotalArea [deg^2]']
+    
+  ##  Schechter fn. parameterisation of the z~3 Reddy luminosity fn., Table 7 of https://arxiv.org/pdf/0706.4091.pdf 
   stats['LBG']['schechter']['phi_star']  = 1.66e-3    ## [\phi*] = [h_70/Mpc]^3 per mag for M*_AB(1700 \AA).
   stats['LBG']['schechter']['M_star']    =  -20.84
   stats['LBG']['schechter']['alpha']     =   -1.57
 
-  ## Convert phi_star to (h_100 / Mpc)^3 per mag.
-  stats['LBG']['schechter']['phi_star'] *= (7. / 10.) ** 3.
-  """
+  ##  Schechter fn. parameterisation of the z~2 Reddy luminosity fn., Table 7 of https://arxiv.org/pdf/0706.4091.pdf                                                                                                                   
+  stats['BX']['schechter']['phi_star']   = 1.74e-3     ## [\phi*] = [h_70/Mpc]^3 per mag for M*_AB(1700 \AA).                                                                                                                           
+  stats['BX']['schechter']['M_star']     =  -20.97
+  stats['BX']['schechter']['alpha']      =   -1.84
+
+  ##  Convert phi_star to (h_100 / Mpc)^3 per mag.
+  stats['LBG']['schechter']['phi_star'] *= (10. / 7.) ** 3.
+  stats['BX']['schechter']['phi_star']  *= (10. / 7.) ** 3.
+  
+  for survey in ['BX', 'LBG']:
+    if printit:
+      print "\n\n%s Survey Specifications:\n" % survey
+
+    for k, v in stats[survey].iteritems():
+      print "%s \t\t %s" % (k.ljust(20), v)
+  
   return  stats
 
 
 if __name__ == "__main__":
-  print '\n\nWelcome to Reddy.py'
+  print('\n\nWelcome to the Reddy (Schechter) calculator.\n\n')
 
-  stats = samplestats(printit = True)
+  stats = samplestats(mag=22.5, printit = False)
 
-  print '\n\nDone.\n\n'
+  print('\n\nDone.\n\n')
 
