ff525dc04c2b018ae86c89fd1a20027a007690c1
Mike Wilson <mjw@roe.ac.uk>
2019-03-21 12:18:22 -0700

    Euclid and other filter load.

--- dropouts/prep_filters.py
+++ dropouts/prep_filters.py
@@ -6,13 +6,15 @@ import  astropy.constants as const
 
 
 def prep_filters(names=['LSST', 'VIDEO'], normed=False):
+  root     = os.environ['LBGCMB'] + '/dropouts/filters/'
+  names    = [x.upper() for x in names]
   filters  = collections.OrderedDict()                                              ## Generate the required filters; Euclid: Y, J and H.                    
 
   if 'LSST' in names:
     for band in ['u', 'g', 'r', 'i', 'z', 'y']:
       ppkey  = r"LSST-$" + "%s" % band + r"$"                                       ## Pretty print version of key.  
 
-      fname  = os.environ['LBGCMB'] + '/dropouts/filters/lsst/total_%s.dat' % band
+      fname  = root + 'lsst/total_%s.dat' % band
 
       ## Check transmission in energy or number;  lsst filters in nanometres.
       ## Lsst throughput (photon counting) in wavelength; [0 to 1].
@@ -35,7 +37,7 @@ def prep_filters(names=['LSST', 'VIDEO'], normed=False):
       for band in ['Y', 'J', 'H', 'K']:
         ppkey  = r"VIDEO-$" + "%s" % band + r"$"                                       ## Pretty print version of key.
 
-        fname  = "filters/video/%s.txt" % band
+        fname  = root + "video/%s.txt" % band
         data   = np.loadtxt(fname)
 
         ls     = data[:,0]
@@ -54,7 +56,7 @@ def prep_filters(names=['LSST', 'VIDEO'], normed=False):
 
     for band in ['B', 'V']:
       ppkey  = r"SUBARU-$" + "%s" % band + r"$"
-      fname  = root + "/filters/subaru/%s.pb" % band
+      fname  = root + "subaru/%s.pb" % band
       data   = np.loadtxt(fname)
 
       ls     = data[:,0]
@@ -68,12 +70,32 @@ def prep_filters(names=['LSST', 'VIDEO'], normed=False):
 
     names.remove('SUBARU')
 
+  if 'EUCLID' in names:
+    ##  https://arxiv.org/pdf/1710.09585.pdf
+    print('Adding Euclid filters (VIS (riz), Y, J, H).')
+
+    for band in ['VIS', 'Y', 'J', 'H']:
+      ppkey  = r"EUCLID-$" + "%s" % band + r"$"
+      fname  = root + "euclid/%s.pb" % band
+      data   = np.loadtxt(fname)
+
+      ls     = data[:,0] ## Angstroms. 
+      vs     = (1.e10 / ls) * const.c.to('m/s').value
+
+      if normed:
+        filters[band] = {'ppkey': ppkey, 'fname': fname, 'ls': ls, 'vs': vs, 'Ts': data[:,1] / data[:,1].max()}
+
+      else:
+        filters[band] = {'ppkey': ppkey, 'fname': fname, 'ls': ls, 'vs': vs, 'Ts': data[:,1]}
+
+    names.remove('EUCLID')
+
   if 'JKC' in names:
     print('Adding JKC filters (I).')
 
     for band in ['I']:
       ppkey  = r"JKC-$" + "%s" % band + r"$"
-      fname  = root + "/filters/jkc/%s.pb" % band
+      fname  = root + "jkc/%s.pb" % band
       data   = np.loadtxt(fname)
 
       ls     = data[:,0]
@@ -97,20 +119,24 @@ def prep_filters(names=['LSST', 'VIDEO'], normed=False):
 if __name__ == "__main__":
   import pylab as pl
 
-  print("\n\nWelcome to prep-filters.\n\n")
 
-  filters = prep_filters()
+  print('\n\nWelcome to prep-filters.\n\n')
+
+  filters = prep_filters(['Euclid'])
 
   for i, band in enumerate(filters.keys()):
-    print band
+    print(band)
 
     pl.plot(filters[band]['ls'], filters[band]['Ts'], label = filters[band]['ppkey'])
 
-  ## for filter in filters.values():
-  ##  print filter['ppkey']
+  ##  for filter in filters.values():
+  ##    print filter['ppkey']
 
-  pl.legend(ncol=2)
+  
 
-  pl.savefig('plots/lsst-filters.pdf')
+  pl.legend(loc=2, ncol=4, frameon=False)
+
+  pl.ylim(0.0, 1.2)
+  pl.savefig('plots/filters.pdf')
 
   print("\n\nDone.\n\n")
